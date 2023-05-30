8450a0470d711015abaec5589cbb61d4c0a16c67
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-05-30 05:21:49 +0000

    datatypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version/ standards of JS
+
+// alert(3+3)  // we are using nodejs and not browser so this won't work here.
+
+
+let name = 'developerCadet' //string datatype
+let age = 25 //number datatype
+let isLoggedIn = false //boolean datatype
+
+// Primitive data types
+/* 
+string - ""
+number - 2 to power 53
+bigint
+boolean - true/false
+null - standalone value/empty
+undefined - when values are not defined but variable is declared
+symbol - used mostly in react for uniqueness 
+*/
+
+
+// objects
+/*
+
+*/ 
