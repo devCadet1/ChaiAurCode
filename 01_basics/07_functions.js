a0530622f05186e26f6d8403bf115eb2d626503f
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-06-06 18:28:00 +0000

    simple calc app in JS using functions

--- 01_basics/07_functions.js
+++ 01_basics/07_functions.js
@@ -1,12 +1,42 @@
 // A function is a block of code that performs a specific task
 
-const a = 12;
-const b = 4;
+// const a = 12;
+// const b = 4;
 
-function sum(a,b) {
-    const sum = a + b
-    console.log(`The sum of a and b is ${sum}`);
+// function sum(a,b) {
+//     const sum = a + b
+//     console.log(`The sum of a and b is ${sum}`);
+// }
+
+
+// sum(a,b)
+
+// calc
+
+const n1 = 4;
+const n2 = 6;
+
+function sum(n1,n2) {
+    console.log(`the sum is ${n1 + n2}`);
 }
 
+sum(n1,n2)
 
-sum(a,b)
+function sub(n1,n2) {
+    console.log(`The difference is ${n1-n2}`);
+}
+
+sub(n1,n2)
+
+function mul(n1,n2) {
+    console.log(`The product is ${n1*n2}`);
+}
+
+mul(n1,n2)
+
+
+function div(n1,n2) {
+    console.log(`The division is ${n1/n2}`);
+}
+
+div(n1,n2)
