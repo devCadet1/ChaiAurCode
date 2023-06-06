c1f2a3a829e5801ad88b76d793343882f4be400a
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-06-06 18:23:21 +0000

    Functions - basics

---
+++ 01_basics/07_functions.js
@@ -0,0 +1,12 @@
+// A function is a block of code that performs a specific task
+
+const a = 12;
+const b = 4;
+
+function sum(a,b) {
+    const sum = a + b
+    console.log(`The sum of a and b is ${sum}`);
+}
+
+
+sum(a,b)
