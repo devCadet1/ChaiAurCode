2d9e4d61afb983253987b14c23f8dbc2c8fec71f
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-06-12 20:37:51 +0000

    arrow functions

---
+++ codingShuttle/callbackandArrow.js
@@ -0,0 +1,21 @@
+// function expression (normal function)
+function add(n,m) {
+    return n + m
+}
+
+console.log(add(2,3))   // 5
+
+
+// Arrow Function  () => {}
+
+const addition = (a , b) => {
+    console.log( a + b    )
+}
+
+addition(4,4)   // 8
+
+// more ways to write a ArrowFunc
+
+const square = (num) => num*num
+
+console.log(square(4))
