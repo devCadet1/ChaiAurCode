055b5c8ee16c32c79e12e596758f5b73e35ea582
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-06-14 18:26:39 +0000

    AsyncFunctions in JS

---
+++ codingShuttle/AsyncFunctions.js
@@ -0,0 +1,10 @@
+// Sync functions or code is written as
+
+console.log('Print');
+console.log('Hello');
+console.log('World');
+//  here the code will run according to its sequence. This is called a regular code or sync code 
+
+// Async is something which would wait for a particular function to run and then a block of code will run after the function is executed
+
+
