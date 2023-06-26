a82a41d4647d90e18f127c0c8fa2452724abd457
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-06-26 18:08:27 +0000

    Learning Objects - basics

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,19 @@
+//  singleton
+
+// Object.create   // connstructor method creates a Singleton object
+
+//  object literals
+
+
+const JsUser = {
+    name: "satyam",
+    age: 25,
+    sex: "male",
+    city: "thane"
+
+}
+
+console.log(JsUser.name)    // satyam
+console.log(JsUser.age)     // 25
+console.log(JsUser.city)    // thane
+console.log(JsUser.sex)     // male
