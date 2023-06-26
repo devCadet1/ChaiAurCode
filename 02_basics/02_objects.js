c18fad8960c970c1145492873600053f0669f8b2
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-06-26 18:20:39 +0000

    Another way to access an Object using obj[" "]

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -6,7 +6,7 @@
 
 
 const JsUser = {
-    name: "satyam",      // 'name' is a 'key' and 'satyam' is the 'value'
+    name: "satyam",      // 'name' is a 'key' and 'satyam' is the 'value'     // here name is considered as a string.... i.e.  "name" : "satyam" 
     age: 25,
     sex: "male",
     city: "thane",
@@ -20,3 +20,12 @@ console.log(JsUser.city)    // thane
 console.log(JsUser.sex)     // male
 console.log(JsUser.isWorking);  // true
 console.log(JsUser.lastLoginDays);      // [ 'monday', 'Wednesday', 'Friday' ]
+
+// Another way to access the objects
+
+console.log(JsUser["name"])    // satyam
+console.log(JsUser["age"])     // 25
+console.log(JsUser["city"]);   // thane
+console.log(JsUser["isWorking"]);   // true
+console.log(JsUser["sex"]);     // male
+console.log(JsUser["lastLoginDays"]);     // [ 'monday', 'Wednesday', 'Friday' ]
