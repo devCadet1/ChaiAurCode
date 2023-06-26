952b488051dad7c76698eda60b8f1d372023b982
SatyamK <55749802+devCadet1@users.noreply.github.com>
2023-06-26 18:16:10 +0000

    moving ahead with Objects

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -6,14 +6,17 @@
 
 
 const JsUser = {
-    name: "satyam",
+    name: "satyam",      // 'name' is a 'key' and 'satyam' is the 'value'
     age: 25,
     sex: "male",
-    city: "thane"
-
+    city: "thane",
+    isWorking: true,     // boolean values can be added in object
+    lastLoginDays: ["monday", "Wednesday", "Friday"]    // we can also include arrays in the Object
 }
 
 console.log(JsUser.name)    // satyam
 console.log(JsUser.age)     // 25
 console.log(JsUser.city)    // thane
 console.log(JsUser.sex)     // male
+console.log(JsUser.isWorking);  // true
+console.log(JsUser.lastLoginDays);      // [ 'monday', 'Wednesday', 'Friday' ]
